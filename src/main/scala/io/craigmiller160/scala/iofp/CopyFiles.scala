e513e17acdf304b40aa1f41512957e809e2808a3
Craig Miller <craigmiller160@gmail.com>
2022-03-06 14:17:38 -0500

    More progress with copy files

--- src/main/scala/io/craigmiller160/scala/iofp/CopyFiles.scala
+++ src/main/scala/io/craigmiller160/scala/iofp/CopyFiles.scala
@@ -2,10 +2,27 @@ package io.craigmiller160.scala.iofp
 
 import cats.effect.{IO, Resource}
 
-import java.io.{File, FileInputStream, FileOutputStream}
+import java.io.{File, FileInputStream, FileOutputStream, InputStream, OutputStream}
 
 object CopyFiles {
-  def copy(origin: File, destination: File): IO[Long] = ???
+  def copy(origin: File, destination: File): IO[Long] =
+    inputOutputStream(new File("./build.sbt"), new File("./build2.sbt")).use {
+      case (in, out) => transfer(in, out)
+    }
+
+  def transfer(inStream: FileInputStream, outStream: FileOutputStream): IO[Long] =
+    transmit(inStream, outStream, new Array[Byte](1024 * 10), 0L)
+
+  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
+    for {
+      amount <- IO.blocking(origin.read(buffer, 0, buffer.length))
+      count <- if (amount > -1) {
+        IO.blocking(destination.write(buffer, 0, amount))
+          .flatMap(_ => transmit(origin, destination, buffer, acc + amount))
+      } else {
+        IO.pure(acc)
+      }
+    } yield count
 
   def inputStream(file: File): Resource[IO, FileInputStream] =
     Resource.make {
