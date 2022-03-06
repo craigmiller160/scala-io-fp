8951941a5cfe61ea67c69471131d212d498c7cc0
Craig Miller <craigmiller160@gmail.com>
2022-03-06 13:37:49 -0500

    Cleaning up initial code

--- src/main/scala/io/craigmiller160/scala/iofp/Runner.scala
+++ src/main/scala/io/craigmiller160/scala/iofp/Runner.scala
@@ -3,12 +3,7 @@ package io.craigmiller160.scala.iofp
 import cats._
 import cats.implicits._
 import cats.effect.{ExitCode, IO, IOApp}
-import io.craigmiller160.scala.iofp.db.Database
-import org.slf4j.LoggerFactory
 
 object Runner extends IOApp {
-  val log = LoggerFactory.getLogger(getClass)
-  log.info("Starting")
-  val ds = Database.getDataSource
   override def run(args: List[String]): IO[ExitCode] = ???
 }
