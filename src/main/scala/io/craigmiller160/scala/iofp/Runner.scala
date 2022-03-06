c40e765047e71d9e57a0b15e74f386c8d4faa17c
Craig Miller <craigmiller160@gmail.com>
2022-03-06 13:16:37 -0500

    Beginning setup work

--- src/main/scala/io/craigmiller160/scala/iofp/Runner.scala
+++ src/main/scala/io/craigmiller160/scala/iofp/Runner.scala
@@ -1,5 +1,9 @@
 package io.craigmiller160.scala.iofp
 
-object Runner extends App {
-  println("Hello World")
+import cats._
+import cats.implicits._
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Runner extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = ???
 }
