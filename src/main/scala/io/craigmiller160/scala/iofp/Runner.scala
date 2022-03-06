952029614b07bbaeccbb796f2bf329accaf468eb
Craig Miller <craigmiller160@gmail.com>
2022-03-06 14:05:17 -0500

    Did some basic IO practice in the runner

--- src/main/scala/io/craigmiller160/scala/iofp/Runner.scala
+++ src/main/scala/io/craigmiller160/scala/iofp/Runner.scala
@@ -6,7 +6,12 @@ import cats.effect._
 
 object Runner extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
-    IO.println("Hello World")
+    IO.println("Hello")
+      .flatMap(_ => IO.println("World"))
+      .flatMap(_ => IO.delay(() => "FooBar"))
+      .flatMap(msgFn => IO.println(msgFn()))
+      .flatMap(_ => IO.blocking(() => "ABC"))
+      .flatMap(msgFn => IO.println(msgFn()))
       .as(ExitCode.Success)
   }
 }
