8268b49e548c8f94b8f5b7eb57fc65859afad091
Craig Miller <craigmiller160@gmail.com>
2022-03-06 16:09:03 -0500

    Stubbing out balance service logic

--- src/main/scala/io/craigmiller160/scala/iofp/Runner.scala
+++ src/main/scala/io/craigmiller160/scala/iofp/Runner.scala
@@ -1,11 +1,18 @@
 package io.craigmiller160.scala.iofp
 
+import cats._
 import cats.effect._
-import io.craigmiller160.scala.iofp.services.PeopleQueryService
+import cats.implicits._
+import io.craigmiller160.scala.iofp.services.{BalanceService, PeopleQueryService}
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object Runner extends IOApp {
+  private implicit def unsafeLogger[F[_]: Sync] = Slf4jLogger.getLogger[F]
+
   override def run(args: List[String]): IO[ExitCode] = {
-    PeopleQueryService.queryForPeople()
+    BalanceService.getBalance("Bob Saget")
+      .flatMap(balance => Logger[IO].info(s"Balance: $$$balance"))
       .as(ExitCode.Success)
   }
 
--- src/main/scala/io/craigmiller160/scala/iofp/services/BalanceService.scala
+++ src/main/scala/io/craigmiller160/scala/iofp/services/BalanceService.scala
@@ -1,7 +1,9 @@
 package io.craigmiller160.scala.iofp.services
 
+import cats.effect.IO
+
 object BalanceService {
-  def getBalance(name: String) = {
+  def getBalance(name: String): IO[Double] = {
     ???
   }
 }
