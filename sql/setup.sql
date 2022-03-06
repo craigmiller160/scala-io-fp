d0758d5496ceb00ab423be1a769833bc2595cb29
Craig Miller <craigmiller160@gmail.com>
2022-03-06 12:50:49 -0500

    More progress with setup schema

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,17 +1,21 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS transaction_type;
+
 CREATE TABLE transaction_type (
     id UUID NOT NULL,
     type_name VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE PEOPLE (
+CREATE TABLE people (
     id UUID NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE TRANSACTIONS (
+CREATE TABLE transactions (
     id UUID NOT NULL,
     type_id UUID NOT NULL,
     person_id UUID NOT NULL,
@@ -20,4 +24,14 @@ CREATE TABLE TRANSACTIONS (
 );
 
 INSERT INTO people (id, first_name, last_name)
-VALUES (gen_random_uuid(), 'Bob', 'Saget');
+VALUES (gen_random_uuid(), 'Bob', 'Saget'), (gen_random_uuid(), 'Jane', 'Foster');
+
+INSERT INTO transaction_type (id, type_name)
+VALUES (gen_random_uuid(), 'Withdrawal'), (gen_random_uuid(), 'Deposit');
+
+WITH bob_id AS (SELECT id FROM people WHERE first_name = 'Bob' AND last_name = 'Saget'),
+     jane_id AS (SELECT id FROM people WHERE first_name = 'Jane' AND last_name = 'Foster'),
+     withdrawal_id AS (SELECT id FROM transaction_type WHERE type_name = 'Withdrawal'),
+     deposit_id AS (SELECT id FROM transaction_type WHERE type_name = 'Deposit')
+INSERT INTO transactions (id, type_id, person_id, amount)
+VALUES (gen_random_uuid(), deposit_id, bob_id, 1000);
