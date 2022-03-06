dd7ec8ef72e95e2ce791f16f4b9f7053efb579c8
Craig Miller <craigmiller160@gmail.com>
2022-03-06 13:01:08 -0500

    Working on new procedure

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS people;
 DROP TABLE IF EXISTS transaction_type;
+DROP PROCEDURE IF EXISTS transaction_load;
 
 CREATE TABLE transaction_type (
     id UUID NOT NULL,
@@ -28,3 +29,17 @@ VALUES (gen_random_uuid(), 'Bob', 'Saget'), (gen_random_uuid(), 'Jane', 'Foster'
 
 INSERT INTO transaction_type (id, type_name)
 VALUES (gen_random_uuid(), 'Withdrawal'), (gen_random_uuid(), 'Deposit');
+
+CREATE PROCEDURE transaction_load () LANGUAGE plpgsql
+    AS $$
+    DECLARE
+        bob_id UUID;
+        jane_id UUID;
+        withdrawal_id UUID;
+        deposit_id UUID;
+    BEGIN
+        SELECT id INTO bob_id FROM people WHERE first_name = 'Bob' AND last_name = 'Saget';
+        SELECT id INTO jane_id FROM people WHERE first_name = 'Jane' AND last_name = 'Foster';
+        SELECT id INTO withdrawal_id FROM transaction_type WHERE type_name = 'Withdrawal';
+        SELECT id INTO deposit_id FROM transaction_type WHERE type_name = 'Deposit';
+    END $$;
