e316cf8df9be0a31a8cc8283b6de452b064c54bb
Craig Miller <craigmiller160@gmail.com>
2022-03-06 14:08:27 -0500

    Working on copy files logic

--- src/main/scala/io/craigmiller160/scala/iofp/CopyFiles.scala
+++ src/main/scala/io/craigmiller160/scala/iofp/CopyFiles.scala
@@ -1,8 +1,25 @@
 package io.craigmiller160.scala.iofp
 
-import cats.effect.IO
-import java.io.File
+import cats.effect.{IO, Resource}
+
+import java.io.{File, FileInputStream, FileOutputStream}
 
 object CopyFiles {
   def copy(origin: File, destination: File): IO[Long] = ???
+
+  def inputStream(file: File): Resource[IO, FileInputStream] =
+    Resource.make {
+      IO.blocking(new FileInputStream(file))
+    } { stream =>
+      IO.blocking(stream.close()).handleErrorWith(_ => IO.unit) // TODO any way to improve error handling?
+    }
+
+  def outputStream(file: File): Resource[IO, FileOutputStream] =
+    Resource.make {
+      IO.blocking(new FileOutputStream(file))
+    } { stream =>
+      IO.blocking(stream.close()).handleErrorWith(_ => IO.unit) // TODO any way to improve error handling?
+    }
+
+
 }
